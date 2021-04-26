a5a2f543ceeeece730214c38dda7dcc252f2e312
dvadee <36699438+dvadee@users.noreply.github.com>
2021-04-26 10:19:31 +0300

    erase

--- common.js
+++ common.js
@@ -2,11 +2,6 @@ import 'bootstrap';
 import 'block-ui';
 import 'jquery-uniform';
 import '@/core/jquery/jquery';
-import '@/base/js/limitlessApp';
-import {
-  initLimitless,
-  initUniformDefaults,
-} from './base/js/limitlessAppCustom';
 import Dashboard from './blocks/dashboard/dashboard';
 import Notifications from './core/notifications/notifications';
 import Tooltip from './core/tooltip/tooltip';
@@ -15,12 +10,10 @@ import Schedules from '@/core/schedules/schedules';
 import ErrorHandler from '@/core/error/handler/error-handler';
 
 ErrorHandler.init();
-initUniformDefaults();
 DataTable.initCustomApi();
 
 document.addEventListener('DOMContentLoaded', () => {
   Dashboard.init();
-  initLimitless();
   Schedules.init();
   Notifications.init();
   Tooltip.init();
